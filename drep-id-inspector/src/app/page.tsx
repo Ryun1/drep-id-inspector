391686026d2f35b210a53202bfc2dcdace64a705
Ryan Williams <ryan.williams@intersectmbo.org>
2025-01-26 21:39:33 +0000

    improve clarity with comments and functions

--- drep-id-inspector/src/app/page.tsx
+++ drep-id-inspector/src/app/page.tsx
@@ -7,55 +7,100 @@ export default function Home() {
   const [features, setFeatures] = useState<string[] | null>(null);
   const [placeholder, setPlaceholder] = useState("Enter DRep ID");
 
-  const isValidDRepID = (value: string): boolean => {
+  const isValidDRepID = (bechID: string): boolean => {
     // DRep ID regex pattern
     // they start with drep1 (at length of 56 or 58) or drep_script1 (at length of 63)
     const drepIDPattern = /^(drep1[a-zA-Z0-9]{51,53}|drep_script1[a-zA-Z0-9]{52})$/;
-    return drepIDPattern.test(value);
+    return drepIDPattern.test(bechID);
   };
+
+  const decodeDRepIDToHex = (bechID: string): string => {
+    // decode DRep ID from Bech32 to hex
+    const bech32 = require('bech32-buffer');
+    const decodedBytes = bech32.decode(bechID).data;
+    const hex = Buffer.from(decodedBytes).toString('hex');
+    return hex;
+  };
+
+  const isHexIDCIP105 = (hexID: string): boolean => {
+    // Use the length of the hex ID to determine if CIP-105 or CIP-129?
+    // if length is 56 then CIP-105, if 58 then CIP-129
+    if (hexID.length === 56) {
+      return true;
+    } else if (hexID.length === 58) {
+      return false;
+    }
+    throw new Error('Invalid hex encoded DRep ID length. Must be 56 or 58 characters.');
+  }
+
+  const isBechIDScriptBased = (bech32: string, isCIP105: boolean): boolean => {
+    // Is DRep ID key-based or script-based?
+    // Use CIP105 bech32 prefix to determine if script-based
+    const hexEncodedID = decodeDRepIDToHex(bech32);
+    if (bech32.startsWith("drep_script1")) {
+      return true;
+      // if CIP-129 and 23 then its script-based
+    } else if (!isCIP105 && hexEncodedID.startsWith("23")) {
+      return true;
+      // else is CIP105 key-based OR is CIP-129 and 22 then its key-based
+    } else {
+      return false;
+    }
+  }
+
+  const hexToBech32 = (hexID: string, isScript?: boolean): string => {
+    const bech32 = require('bech32-buffer');
+    const isCIP105 = isHexIDCIP105(hexID);
+    // if we are encoding a CIP105 hex ID
+    // use different prefixes for CIP105 script-based and key-based
+    if (isCIP105) {
+      if (isScript) {
+        return bech32.encode("drep_script", Buffer.from(hexID, 'hex')).toString();
+      } else {
+        return bech32.encode("drep", Buffer.from(hexID, 'hex')).toString();
+      }
+    // else we are encoding a CIP129 hex ID
+    } else {
+      return bech32.encode("drep", Buffer.from(hexID, 'hex')).toString();
+    }
+  }
+
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setInput(value);
 
+    // exit function if input is not a bech32 DRep ID
     if (!isValidDRepID(value)) {
       setFeatures(null);
       return;
     }
 
+    const bechEncodedID = value;
+    const hexEncodedID = decodeDRepIDToHex(bechEncodedID);
+
+    const isCIP105 = isHexIDCIP105(hexEncodedID);
+    const isScriptBased = isBechIDScriptBased(bechEncodedID, isCIP105);
+
     const features = [];
 
-    // decode DRep ID from Bech32
-    const bech32 = require('bech32-buffer');
-    const decodedBech32Bytes = bech32.decode(value).data;
-    const hexEncodedID = Buffer.from(decodedBech32Bytes).toString('hex');
-
-    let isCIP105;
-    let isScriptBased;
-
-    // Is DRep ID CIP-105 or CIP-129?
-    if (hexEncodedID.length === 56) {
-      isCIP105 = true;
+    // Add to list of features the standard used
+    if (isCIP105) {
       features.push("Standard: CIP-105");
-    } else if (hexEncodedID.length === 58) {
-      isCIP105 = false;
+    } else if (!isCIP105) {
       features.push("Standard: CIP-129");
     }
 
-    // Is DRep ID key-based or script-based?
-    if (value.startsWith("drep_script1")) {
-      isScriptBased = true;
+    // Add to list if the ID is script based or key based
+    if (isScriptBased) {
       features.push("Type: Script-based");
-      // is CIP129 and hex starts with 23
-    } else if (!isCIP105 && hexEncodedID.startsWith("23")) {
-      isScriptBased = true;
+    } else if (isScriptBased) {
       features.push("Type: Script-based");
     } else {
-      isScriptBased = false;
       features.push("Type: Key-based");
     }
 
-    // Add hex to features
+    // Add the hex representation to the outputted features
     if (isCIP105) {
       features.push(`Hex: ${'  '+hexEncodedID}`);
     } else {
@@ -63,18 +108,20 @@ export default function Home() {
     }
 
     // Add alternative encoding to features
+    // if provided ID is CIP105 then output CIP129
     if (isCIP105) {
+      // if provided ID is CIP105 then add CIP129 prefix before encoding
+      // if script-based then add 23 prefix, else use 22 prefix
       if (isScriptBased) {
-        features.push(`CIP-129: ${bech32.encode("drep", Buffer.from( '23' + hexEncodedID, 'hex')).toString()}`);
+        features.push(`CIP-129: ${hexToBech32('23' + hexEncodedID)}`);
       } else {
-        features.push(`CIP-129: ${bech32.encode("drep", Buffer.from('22' + hexEncodedID, 'hex')).toString()}`);
+        features.push(`CIP-129: ${hexToBech32('22' + hexEncodedID)}`);
       }
+    // if provided ID is CIP129 then output CIP105 
+    // by removing respective prefix and then encoding
     } else {
-      if (isScriptBased) {
-        features.push(`CIP-105: ${bech32.encode("drep_script1", Buffer.from(hexEncodedID.slice(2), 'hex')).toString()}`);
-      } else {
-        features.push(`CIP-105: ${bech32.encode("drep", Buffer.from(hexEncodedID.slice(2), 'hex')).toString()}`);
-      }
+      // remove the 22 or 23 prefix
+      features.push(`CIP-105: ${hexToBech32(hexEncodedID.slice(2), isScriptBased)}`);
     }
 
     setFeatures(features);
